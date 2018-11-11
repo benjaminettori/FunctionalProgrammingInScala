e05ebc1f37a86fb2c2d5b45546c37a32c3b13e80
Benjamin Ettori <mistor_ben@yahoo.com>
2018-11-11 16:30:19 -0500

    Add tailrec trait.

--- src/main/scala-2.12/Chapter13.scala
+++ src/main/scala-2.12/Chapter13.scala
@@ -1,4 +1,6 @@
-import Chapter11.Mon
+import Chapter11._
+
+import scala.annotation.tailrec
 
 object Chapter13 {
   case class Player(name: String, score: Int)
@@ -64,31 +66,53 @@ object Chapter13 {
     PrintLine(getMessage(winner(p1, p2)))
   }
 
-  trait IO[A] { self =>
-    def run : A
-    def map[B](f : A => B) : IO[B] = new IO[B] {
-      def run : B = f(self.run)
-    }
-    def flatMap[B](f : A => IO[B]) : IO[B] = f(self.run)
+  sealed trait TailRec[A] {
+    @tailrec
+    final def run[A](ioa :TailRec[A]) : A = ioa match {
+        case Return(v) => v
+        case Suspend(res) => res()
+        case FlatMap(x, f: (Any => TailRec[A])) => x match {
+          case Return(a) => run(f(a))
+          case Suspend(r) => run(f(r()))
+          case FlatMap(y, g : (Any => TailRec[Any])) => run(y flatMap (a =>  g(a) flatMap f))
+        }
+      }
+    def map[B](f : A => B) : TailRec[B] = flatMap(f andThen(x => Return(x)))
+    def flatMap[B](f : A => TailRec[B]) : TailRec[B] = FlatMap(this, f)
   }
 
-  object IO extends Mon[IO] {
-    override def flatMap[A, B](fa: IO[A])(f: A => IO[B]): IO[B] = fa flatMap f
-    override def unit[A](a: => A): IO[A] = new IO[A] {
-      override def run: A = a
+  // Class implementations of TailRec
+  case class Return[A](a: A) extends TailRec[A]
+  case class Suspend[A](resume: () => A) extends TailRec[A]
+  case class FlatMap[A, B](ioa: TailRec[A], f: A => TailRec[B]) extends TailRec[B]
+
+  object TailRec extends Mon[TailRec] {
+    override def flatMap[A, B](fa: TailRec[A])(f: A => TailRec[B]): TailRec[B] = fa flatMap f
+    override def unit[A](a: => A): TailRec[A] = new TailRec[A] {
+
     }
-    def apply[A](a: => A): IO[A] = unit(a)
+    def apply[A](a: => A): TailRec[A] = unit(a)
   }
 
   def fahrenheitToCelsius(f: Double) : Double = (f - 32) * 5.0/9.0
 
-  def ReadLine : IO[String] = IO {scala.io.StdIn.readLine}
-  def PrintLn(msg: String) : IO[Unit] = IO {println(msg)}
+  def ReadLine : TailRec[String] = TailRec {scala.io.StdIn.readLine}
+  def PrintLn(msg: String) : TailRec[Unit] = TailRec {println(msg)}
 
   // This for comprehension works because the Monad is IO
-  def converter : IO[Unit] = for {
+  def converter : TailRec[Unit] = for {
     _ <- PrintLn("Enter a temperatures in Fahrenheit: ")
     temp <- ReadLine.map(_.toDouble)
     _ <- PrintLn(fahrenheitToCelsius(temp).toString)
   } yield ()
+
+  def printline(s: String) : TailRec[Unit] = {
+    Suspend(() => Return(println(s)))
+  }
+  
+  def main(args: Array[String]): Unit = {
+    println("Test")
+  }
+
+
 }
--- src/main/scala-2.12/Test.sc
+++
@@ -1,6 +0,0 @@
-val myString = "Hello"
-val regex = myString.r
-
-regex.pattern.matcher("Ben").matches()
-regex.pattern.matcher("Hello Goodbye").matches()
-regex.regex
